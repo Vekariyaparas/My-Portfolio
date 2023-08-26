bfac9d12bca81438c946c035a7d391cd85d08b37
Vekariyaparas <parasvekariya2000@gmail.com>
2023-08-26 09:50:09 +0530

    hy

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+    $(document).ready(function(){
+        $(".fa-bars").click(function(){
+
+            $(".navbar").slideToggle(500);
+        });
+        $("#slide").cycle("fade");
+    })
+
+
+
+
+
+
+
+
+
